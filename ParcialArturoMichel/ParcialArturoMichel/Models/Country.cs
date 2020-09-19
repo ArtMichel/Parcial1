6c1b67c770566a00e1da5f3d31d286d216c3c356
ArtMichel <a2019116758@estudiantes.upsa.edu.bo>
2020-09-19 12:20:09 -0400

    Creado el modelo

---
+++ ParcialArturoMichel/ParcialArturoMichel/Models/Country.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ParcialArturoMichel.Models
+{
+    public class Country
+    {
+        [Key]
+        [Required]
+        [StringLength(3, ErrorMessage = "El Alphacode debe ser de 3 caracteres",MinimumLength = 3)]
+        public string Alpha3code { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Revise su region", MinimumLength = 2)]
+        public string Region { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Nombre invalido", MinimumLength = 2)]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, 99999999)]
+        public int Area { get; set; }
+        [Required]
+        public List<string> Callingcodes { get; set; }
+        [Required]
+        public List<string> Languages { get; set; }
+        [Required]
+        [Url]
+        public string Flag { get; set; }
+    }
+}
